256cc250b26b5f476aa0668b5134d90014eee0a8
Dane Powell <git@danepowell.com>
2023-02-24 13:53:24 -0800

    Initial commit

---
+++ src/War.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Danepowell\MutationExample;
+
+class War
+{
+
+    public string $card1;
+    public string $card2;
+
+    /**
+     * Cards can be numeric (2-10) or a face card (J, Q, K, A).
+     */
+    public function __construct(string $card1, string $card2)
+    {
+        $this->card1 = $card1;
+        $this->card2 = $card2;
+    }
+
+    public static function getCardValue(string $card): int {
+        if (!in_array($card, ['J', 'Q', 'K', 'A'])) {
+            return $card;
+        }
+        return match($card) {
+            'J' => 11,
+            'Q' => 12,
+            'K' => 13,
+            'A' => 14
+        };
+    }
+
+    public function announceWinner(): string
+    {
+        $card1value = self::getCardValue($this->card1);
+        $card2value = self::getCardValue($this->card2);
+        if ($card1value > $card2value) {
+            return "Player 1 wins!";
+        }
+
+        if ($card1value < $card2value) {
+            return "Player 2 wins!";
+        }
+
+        return "It's a war!";
+    }
+}
---
+++ tests/Danepowell/MutationExample/WarTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Danepowell\MutationExample;
+
+use PHPUnit\Framework\TestCase;
+
+final class WarTest extends TestCase
+{
+
+    /**
+     * @covers \Danepowell\MutationExample\War::__construct
+     *
+     * @return void
+     */
+    public function testClassConstructor(): void {
+        $war = new War('2', 'A');
+
+        $this->assertSame('2', $war->card1);
+        $this->assertSame('A', $war->card2);
+    }
+
+    /**
+     * @covers       \Danepowell\MutationExample\War::getCardValue
+     *
+     * @dataProvider provideTestGetCardValue
+     *
+     * @param string $card
+     * @param int $value
+     *
+     * @return void
+     */
+    public function testGetCardValue(string $card, int $value): void {
+        $this->assertSame($value, War::getCardValue($card));
+    }
+
+    public static function provideTestGetCardValue(): array {
+        return [
+            ['2', 2],
+            ['3', 3],
+            ['4', 4],
+            ['5', 5],
+            ['6', 6],
+            ['7', 7],
+            ['8', 8],
+            ['9', 9],
+            ['10', 10],
+            ['J', 11],
+            ['Q', 12],
+            ['K', 13],
+            ['A', 14]
+        ];
+    }
+
+    /**
+     * @param $card1
+     * @param $card2
+     * @param $expectedWinner
+     *
+     * @dataProvider provideTestAnnounceWinner
+     *
+     * @covers \Danepowell\MutationExample\War::announceWinner
+     * @return void
+     */
+    public function testAnnounceWinner($card1, $card2, $expectedWinner): void {
+        $war = new War($card1, $card2);
+        $this->assertSame($expectedWinner, $war->announceWinner());
+    }
+
+    public static function provideTestAnnounceWinner(): array {
+        return [
+            ['3', '2', 'Player 1 wins!'],
+            ['2', '3', 'Player 2 wins!'],
+            ['2', '2', 'It\'s a war!']
+        ];
+    }
+}
