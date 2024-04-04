035afec2ab51626668d2dfac8c5cd029fcd96a5b
Dane Powell <git@danepowell.com>
2024-04-04 09:50:27 -0700

    phpcs

--- tests/src/WarTest.php
+++ tests/src/WarTest.php
@@ -6,13 +6,13 @@ use PHPUnit\Framework\TestCase;
 
 final class WarTest extends TestCase
 {
-
     /**
      * @covers \Danepowell\MutationExample\War::__construct
      *
      * @return void
      */
-    public function testClassConstructor(): void {
+    public function testClassConstructor(): void
+    {
         $war = new War('2', 'A');
 
         $this->assertSame('2', $war->card1);
@@ -29,11 +29,13 @@ final class WarTest extends TestCase
      *
      * @return void
      */
-    public function testGetCardValue(string $card, int $value): void {
+    public function testGetCardValue(string $card, int $value): void
+    {
         $this->assertSame($value, War::getCardValue($card));
     }
 
-    public static function provideTestGetCardValue(): array {
+    public static function provideTestGetCardValue(): array
+    {
         return [
             ['2', 2],
             ['3', 3],
@@ -61,12 +63,14 @@ final class WarTest extends TestCase
      * @covers \Danepowell\MutationExample\War::announceWinner
      * @return void
      */
-    public function testAnnounceWinner($card1, $card2, $expectedWinner): void {
+    public function testAnnounceWinner($card1, $card2, $expectedWinner): void
+    {
         $war = new War($card1, $card2);
         $this->assertSame($expectedWinner, $war->announceWinner());
     }
 
-    public static function provideTestAnnounceWinner(): array {
+    public static function provideTestAnnounceWinner(): array
+    {
         return [
             ['3', '2', 'Player 1 wins!'],
             ['2', '3', 'Player 2 wins!'],
